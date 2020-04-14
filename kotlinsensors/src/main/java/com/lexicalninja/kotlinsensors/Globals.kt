ccb1c02fab5d89ee1cb344ab52de3a111a46c1cf
Patrick Saxton <patrick@urban-intellect.com>
2020-04-14 13:51:27 -0500

    fix contains

--- kotlinsensors/src/main/java/com/lexicalninja/kotlinsensors/Globals.kt
+++ kotlinsensors/src/main/java/com/lexicalninja/kotlinsensors/Globals.kt
@@ -25,7 +25,7 @@ open class FlagStruct {
     internal var rawFlags: Int = 0
 
     operator fun contains(flagPosition: Int): Boolean {
-        return rawFlags.toLong() and (1L shl flagPosition) == flagPosition.toLong()
+        return rawFlags.toLong() and (1L shl flagPosition) == (1L shl flagPosition)
     }
 
     constructor() {}
